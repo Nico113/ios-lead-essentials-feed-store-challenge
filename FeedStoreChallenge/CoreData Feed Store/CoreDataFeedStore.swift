b86dccaedec41f76d17fcfb41a5c2477b96fa1f5
Nicolas De Maio <nicolasddemaio@gmail.com>
2020-09-24 14:49:13 -0300

    Making container and context properties not optional as CoreDataFeedStore does not work without them

--- FeedStoreChallenge/CoreData Feed Store/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreData Feed Store/CoreDataFeedStore.swift
@@ -10,103 +10,89 @@ import Foundation
 import CoreData
 
 public class CoreDataFeedStore: FeedStore {
-    private let container: NSPersistentContainer?
-    private let context: NSManagedObjectContext?
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
     
     public init(model: String = "FeedStore", bundle: Bundle = Bundle(for: CoreDataFeedStore.self)) throws {
-        guard let mom = NSManagedObjectModel.mergedModel(from: [bundle]) else {
-            container = nil
-            context = nil
-            return
+        if let mom = NSManagedObjectModel.mergedModel(from: [bundle]) {
+            container = NSPersistentContainer(name: model, managedObjectModel: mom)
+            var receivedError: Error?
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                receivedError = error
+            })
+            if let error = receivedError {
+                throw error
+            }
+            context = container.newBackgroundContext()
+        } else {
+            throw NSError(domain: "Model is not found in bundle", code: 0)
         }
-        
-        container = NSPersistentContainer(name: model, managedObjectModel: mom)
-        var receivedError: Error?
-        container?.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            receivedError = error
-        })
-        if let error = receivedError {
-            throw error
-        }
-        context = container?.newBackgroundContext()
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        if let context = context {
-            context.perform {
-                do {
-                    if let currentCache: Cache = try context.fetch(Cache.fetchRequest()).first {
-                        context.delete(currentCache)
-                        do {
-                            try context.save()
-                            completion(.none)
-                        } catch {
-                            completion(error)
-                        }
-                    } else {
+        context.perform { [context] in
+            do {
+                if let currentCache: Cache = try context.fetch(Cache.fetchRequest()).first {
+                    context.delete(currentCache)
+                    do {
+                        try context.save()
                         completion(.none)
+                    } catch {
+                        completion(error)
                     }
-                } catch {
-                    completion(error)
+                } else {
+                    completion(.none)
                 }
+            } catch {
+                completion(error)
             }
-        } else {
-            completion(.none)
         }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        if let context = context {
-            context.perform {
-                do {
-                    var cache: Cache
-                    if let currentCache: Cache = try context.fetch(Cache.fetchRequest()).first {
-                        cache = currentCache
-                    } else {
-                        cache = Cache(context: context)
-                    }
-                    
-                    var feedImages = [NSManagedObject]()
-                    for localFeedImage in feed {
-                        let feedImage = FeedImage(context: context)
-                        
-                        feedImage.id = localFeedImage.id
-                        feedImage.imageDescription = localFeedImage.description
-                        feedImage.location = localFeedImage.location
-                        feedImage.url = localFeedImage.url
-                        
-                        feedImages.append(feedImage)
-                    }
-                    
-                    cache.timestamp = timestamp
-                    cache.items = NSOrderedSet(array: feedImages)
-                    
-                    try context.save()
-                    completion(.none)
-                } catch {
-                    completion(error)
+        context.perform { [context] in
+            do {
+                var cache: Cache
+                if let currentCache: Cache = try context.fetch(Cache.fetchRequest()).first {
+                    cache = currentCache
+                } else {
+                    cache = Cache(context: context)
                 }
+                
+                var feedImages = [NSManagedObject]()
+                for localFeedImage in feed {
+                    let feedImage = FeedImage(context: context)
+                    
+                    feedImage.id = localFeedImage.id
+                    feedImage.imageDescription = localFeedImage.description
+                    feedImage.location = localFeedImage.location
+                    feedImage.url = localFeedImage.url
+                    
+                    feedImages.append(feedImage)
+                }
+                
+                cache.timestamp = timestamp
+                cache.items = NSOrderedSet(array: feedImages)
+                
+                try context.save()
+                completion(.none)
+            } catch {
+                completion(error)
             }
-        } else {
-            completion(NSError())
         }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        if let context = context {
-            context.perform {
-                do {
-                    if let cache: Cache = try context.fetch(Cache.fetchRequest()).first {
-                        completion(.found(feed: cache.local, timestamp: cache.timestamp))
-                    } else {
-                        completion(.empty)
-                    }
-                } catch {
-                    completion(.failure(error))
+        context.perform { [context] in
+            do {
+                if let cache: Cache = try context.fetch(Cache.fetchRequest()).first {
+                    completion(.found(feed: cache.local, timestamp: cache.timestamp))
+                } else {
+                    completion(.empty)
                 }
+            } catch {
+                completion(.failure(error))
             }
-        } else {
-            completion(.empty)
         }
     }
 }
